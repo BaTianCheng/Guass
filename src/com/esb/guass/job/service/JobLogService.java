f2977aa9514d4ad746200b889241d1a3b749efc3
BaTianCheng <cheng_wei_njut@163.com>
2017-09-06 21:33:15 +0800

    增加配置

--- src/com/esb/guass/job/service/JobLogService.java
+++ src/com/esb/guass/job/service/JobLogService.java
@@ -1,8 +1,14 @@
 package com.esb.guass.job.service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
 import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 /**
@@ -14,12 +20,41 @@ public class JobLogService{
 	private static Logger log = Logger.getLogger("com.esb.job");  
 
 	static{
+		// 读取配置文件  
+        ClassLoader cl = LogManager.class.getClassLoader();  
+        InputStream inputStream = null;  
+        if (cl != null) {  
+            inputStream = cl.getResourceAsStream(System.getProperty("user.dir")+"/conf/logging.properties");  
+        } else {  
+            inputStream = ClassLoader  
+                    .getSystemResourceAsStream(System.getProperty("user.dir")+"/conf/logging.properties");  
+        }  
+        java.util.logging.LogManager logManager = java.util.logging.LogManager  
+                .getLogManager();  
+        try {  
+            // 重新初始化日志属性并重新读取日志配置。  
+            logManager.readConfiguration(inputStream);  
+        } catch (SecurityException e) {  
+            System.err.println(e);  
+        } catch (IOException e) {  
+            System.err.println(e);  
+        }  
+		
 		log.setLevel(Level.INFO);  
         
         FileHandler fileHandler;
 		try {
-			fileHandler = new FileHandler("D:/testlog%g.log", true);
+			String userDir = System.getProperty("user.dir");
+			fileHandler = new FileHandler(userDir+"/logs/jobs/joblog-%g.log", true);
 			fileHandler.setLevel(Level.INFO);
+			fileHandler.setFormatter(new Formatter() {
+				public String format(LogRecord record) {
+					SimpleDateFormat sd = new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]");
+					String d = sd.format(new Date());
+					return d + record.getLevel() + ":" + record.getMessage() + "\n";
+				}
+			});
+		          
 			log.addHandler(fileHandler);  
 		} catch (SecurityException e) {
 			// TODO Auto-generated catch block
@@ -28,18 +63,6 @@ public class JobLogService{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}  
-        
-/*
-        fileHandler.setFormatter(new Formatter()  
-                                 {  
-                                    public String format(LogRecord record)  
-                                    {  
-                                        SimpleDateFormat sd = new SimpleDateFormat("[yyyy-MM-dd HH:mm:ss]");    
-                                        String d = sd.format(new Date());    
-                                        return d + record.getLevel() + ":" + record.getMessage() + "/n";  
-                                    }  
-                                 });  
-                                 */
         
 	}
 	
@@ -54,6 +77,13 @@ public class JobLogService{
      */
     public static void error(String msg, Throwable thrown){
     	log.log(Level.SEVERE, msg, thrown);
+    }
+    
+    public static void main(String[] args) {
+    	
+    	
+    	
+    	JobLogService.info("xxx");
     }
 	
 }
